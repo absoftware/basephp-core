418648f450ab992db64af1b59e24f83d03b28799
Ariel Bogdziewicz <ariel.bogdziewicz@gmail.com>
2018-07-30 01:15:32 +0200

    Exposed methods of Request in Controller.

--- sources/Core/Controller.php
+++ sources/Core/Controller.php
@@ -9,7 +9,6 @@
 namespace Base\Core;
 
 use Base\Exceptions\BadRequest;
-use Base\Templates\PhpTemplate;
 use Base\Templates\Template;
 
 /**
@@ -23,45 +22,126 @@ abstract class Controller
      * Current request.
      * @var Request
      */
-    private $request;
+    private $request = null;
 
     /**
      * Interface for template engine.
      * @var Template
      */
-    private $template;
+    private $template = null;
 
     /**
      * Current session.
      * @var Session
      */
-    private $session;
+    private $session = null;
 
     /**
-     * Controller constructor.
+     * Sets request object.
+     * @param Request $request
      */
-    public function __construct()
-    {
-        $this->request = null;
-        $this->template = null;
-        $this->session = null;
-    }
-
     public function setRequestObject(Request $request)
     {
         $this->request = $request;
     }
 
+    /**
+     * Sets session object.
+     * @param Session $session
+     */
     public function setSessionObject(Session $session)
     {
         $this->session = $session;
     }
 
+    /**
+     * Sets template engine.
+     * @param Template $template
+     */
+    public function setTemplateObject(Template $template)
+    {
+        $this->template = $template;
+    }
+
+    /**
+     * Returns true if connection is performed through HTTPS protocol.
+     * @return bool
+     */
+    protected function isHttps(): bool
+    {
+        return $this->request->isHttps();
+    }
+
+    /**
+     * Returns true if request is an AJAX request.
+     * @return bool
+     */
+    protected function isAjax(): bool
+    {
+        return $this->request->isAjax();
+    }
+
+    /**
+     * Protocol.
+     * @return string
+     */
+    public function protocol()
+    {
+        return $this->request->protocol();
+    }
+
+    /**
+     * Full host name.
+     * @return string
+     */
+    public function host()
+    {
+        return $this->request->host();
+    }
+
+    /**
+     * Current port.
+     * @return int
+     */
+    public function port()
+    {
+        return $this->request->port();
+    }
+
+    /**
+     * Request URI which means path and arguments starting from slash character.
+     * @return mixed
+     */
+    public function uri()
+    {
+        return $this->request->uri();
+    }
+
+    /**
+     * Path of request without arguments starting from slash character.
+     * @return mixed|string
+     */
+    public function path()
+    {
+        return $this->request->path();
+    }
+
+    /**
+     * Gets GET param.
+     * @param $name
+     * @return array|float|null|string
+     */
     protected function get($name)
     {
         return $this->request->get($name);
     }
 
+    /**
+     * Gets GET param or throws exception if param is not set.
+     * @param $name
+     * @return array|float|null|string
+     * @throws BadRequest
+     */
     protected function requiredGet($name)
     {
         $param = $this->request->get($name);
@@ -71,12 +151,23 @@ abstract class Controller
         }
         return $param;
     }
-    
+
+    /**
+     * Gets POST param.
+     * @param $name
+     * @return array|float|null|string
+     */
     protected function post($name)
     {
         return $this->request->post($name);
     }
-    
+
+    /**
+     * Gets POST param or throws exception if param is not set.
+     * @param $name
+     * @return array|float|null|string
+     * @throws BadRequest
+     */
     protected function requiredPost($name)
     {
         $param = $this->request->post($name);
@@ -86,32 +177,83 @@ abstract class Controller
         }
         return $param;
     }
-    
+
+    /**
+     * Gets cookie from request.
+     * @param $name
+     * @return null
+     */
     protected function cookie($name)
     {
         return $this->request->cookie($name);
     }
-    
+
+    /**
+     * Sets cookie.
+     * @param $name
+     * @param $value
+     * @param int $lifeTime
+     * @param bool $currentDomainOnly
+     */
     protected function setCookie($name, $value, int $lifeTime = 31536000, bool $currentDomainOnly = false)
     {
         $this->session->setCookie($name, $value, $lifeTime, $currentDomainOnly);
     }
-    
+
+    /**
+     * Gets session variable.
+     * @param $name
+     * @return mixed
+     */
     protected function session($name)
     {
         return $this->session->get($name);
     }
-    
-    protected function setSession($name, $value)
+
+    /**
+     * Sets session variable.
+     * @param $name
+     * @param $value
+     */
+    protected function setSession($name, $value): void
     {
         $this->session->set($name, $value);
     }
-    
+
+    /**
+     * Check existence of session variable.
+     * @param $name
+     * @return bool
+     */
+    protected function issetSession($name): bool
+    {
+        return $this->session->isset($name);
+    }
+
+    /**
+     * Destroys session variable.
+     * @param $name
+     */
+    protected function unsetSession($name): void
+    {
+        $this->session->unset($name);
+    }
+
+    /**
+     * Assigns template variable.
+     * @param $name
+     * @param $value
+     */
     protected function assign($name, $value)
     {
         $this->template->assign($name, $value);
     }
-    
+
+    /**
+     * Returns rendered template.
+     * @param string|null $templateFile
+     * @return string
+     */
     protected function fetch(string $templateFile = null)
     {
         if (!$templateFile)
@@ -122,21 +264,5 @@ abstract class Controller
             $templateFile = $directory . basename($path, ".php") . $this->template->fileExtension();
         }
         return $this->template->fetch($templateFile);
-    }
-    
-    /**
-     * Returns raw content of view for example HTML code
-     * or other content which can be outputted straight into
-     * output buffer or included in template.
-     * @return string Raw content of view.
-     */
-    abstract public function getOutput(): string;
-    
-    /**
-     * Allows using this object as string.
-     */
-    public function __toString(): string
-    {
-        return $this->getOutput();
     }
 }
--- sources/Core/Request.php
+++ sources/Core/Request.php
@@ -8,20 +8,40 @@
  */
 namespace Base\Core;
 
+/**
+ * Class Request delivers all information about request.
+ * @package Base\Core
+ */
 class Request
 {
+    /**
+     * Configuration of ports.
+     * @var Ports|null
+     */
     protected $ports = null;
-    
+
+    /**
+     * Request constructor.
+     * @param Ports|null $ports
+     */
     public function __construct(Ports $ports = null)
     {
         $this->ports = $ports ?? new Ports();
     }
-    
+
+    /**
+     * Request method.
+     * @return string
+     */
     public function method()
     {
         return strtoupper($_SERVER['REQUEST_METHOD']);
     }
-    
+
+    /**
+     * Returns true if connection is performed through HTTPS protocol.
+     * @return bool
+     */
     public function isHttps()
     {
         if (!empty($_SERVER['REQUEST_SCHEME']) && $_SERVER['REQUEST_SCHEME'] == 'https')
@@ -41,22 +61,38 @@ class Request
         
         return false;
     }
-    
+
+    /**
+     * Returns true if request is an AJAX request.
+     * @return bool
+     */
     function isAjax()
     {
         return isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest';
     }
-    
+
+    /**
+     * Protocol.
+     * @return string
+     */
     public function protocol()
     {
         return $this->isHttps() ? "https://" : "http://";
     }
-    
+
+    /**
+     * Full host name.
+     * @return string
+     */
     public function host()
     {
         return strtolower($_SERVER['SERVER_NAME']);
     }
-    
+
+    /**
+     * Current port.
+     * @return int
+     */
     public function port()
     {
         if (isset($_SERVER['SERVER_PORT']))
@@ -69,28 +105,52 @@ class Request
         }
         return $port;
     }
-    
+
+    /**
+     * Request URI which means path and arguments starting from slash character.
+     * @return mixed
+     */
     public function uri()
     {
         return $_SERVER['REQUEST_URI'];
     }
 
+    /**
+     * Path of request without arguments starting from slash character.
+     * @return mixed|string
+     */
     public function path()
     {
         $requestUri = explode("?", $this->uri());
         return is_array($requestUri) && count($requestUri) > 0 ? $requestUri[0] : "/";
     }
-    
+
+    /**
+     * Gets GET param.
+     * @param $name
+     * @return array|float|null|string
+     */
     public function get($name)
     {
         return $this->getVariable($_GET, $name);
     }
 
+    /**
+     * Gets POST param.
+     * @param $name
+     * @return array|float|null|string
+     */
     public function post($name)
     {
         return $this->getVariable($_POST, $name);
     }
-    
+
+    /**
+     * Private method used to process GET and POST variables.
+     * @param $array
+     * @param $name
+     * @return array|float|null|string
+     */
     private function getVariable($array, $name)
     {
         if (isset($array[$name]))
@@ -119,13 +179,23 @@ class Request
         }
         return null;
     }
-    
-    public function escape($string)
+
+    /**
+     * Private method used to escape GET and POST variables.
+     * @param $string
+     * @return string
+     */
+    private function escape($string)
     {
         return get_magic_quotes_gpc() ? stripslashes($string) : $string;
     }
-    
-    public function cookie($name)
+
+    /**
+     * Gets cookie from request.
+     * @param $name
+     * @return string
+     */
+    public function cookie($name): ?string
     {
         if (isset($_COOKIE[$name]))
         {
