8ed52ee0125e78fa0b74489630e20d2570e2e705
Ariel Bogdziewicz <ariel.bogdziewicz@gmail.com>
2018-09-15 04:56:35 +0200

    Changed name of function in provisioning.sh file.

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -9,10 +9,10 @@
 export DEBIAN_FRONTEND=noninteractive
 export PROJECT_DIR=/home/vagrant/www/basephp-core
 
-lib_configuration() {
+core_configuration() {
     echo "Executing composer"
     su - vagrant -s /bin/bash -c "cd ${PROJECT_DIR} && php composer.phar install"
 }
 
 echo "BasePHP Core - Provisioning virtual machine..."
-lib_configuration
+core_configuration
